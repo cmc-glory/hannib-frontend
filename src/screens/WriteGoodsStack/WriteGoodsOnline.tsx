3386398d503554427b3ab99b2006fb4119fc9fce
kauthenticity <kauthenticity@gmail.com>
2022-06-02 22:38:08 +0900

    [feat]: completed goods form-step2:online

--- src/screens/WriteGoodsStack/WriteGoodsOnline.tsx
+++ src/screens/WriteGoodsStack/WriteGoodsOnline.tsx
@@ -1,13 +1,16 @@
-import React, {useCallback} from 'react'
-import {View, Text, ScrollView, StyleSheet} from 'react-native'
+import React, {useCallback, useState} from 'react'
+import {View, Text, TextInput, ScrollView, StyleSheet} from 'react-native'
 import {SafeAreaView} from 'react-native-safe-area-context'
-
+import {Switch} from 'react-native-paper'
 import StackHeader from '../../components/utils/StackHeader'
-import {StepIndicator, NextButton} from '../../components/WriteGoodsStack'
-import {styles as s} from '../../theme'
+
+import {StepIndicator, NextButton, DatePicker, GoodsInput, AdditionalQuestions} from '../../components/WriteGoodsStack'
+import {styles as s, black} from '../../theme'
 
 export const WriteGoodsOnline = () => {
   const onPressNext = useCallback(() => {}, [])
+  const [isEnabled, setIsEnabled] = useState(false)
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState)
   return (
     <SafeAreaView style={{backgroundColor: '#fff', flex: 1}}>
       <StackHeader goBack title="모집폼 작성" />
@@ -16,17 +19,26 @@ export const WriteGoodsOnline = () => {
           <StepIndicator step={2} />
         </View>
         <View style={[s.wrapper]}>
-          <Text style={[s.label]}>주소 정보 수령</Text>
+          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+            <Text style={[s.label]}>주소 정보 수정</Text>
+            <Switch color={black} onValueChange={toggleSwitch} value={isEnabled} style={styles.switch} />
+          </View>
         </View>
         <View style={[s.wrapper]}>
           <Text style={[s.label]}>상품 정보</Text>
+          <GoodsInput />
         </View>
         <View style={[s.wrapper]}>
           <Text style={[s.label]}>추가 질문 사항</Text>
+          <AdditionalQuestions />
         </View>
 
         <View style={[s.wrapper]}>
-          <Text style={[s.label]}>시크릿 폼</Text>
+          <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+            <Text style={[s.label]}>시크릿 폼</Text>
+            <Switch color={black} onValueChange={toggleSwitch} value={isEnabled} style={styles.switch} />
+          </View>
+          <TextInput style={s.input} />
         </View>
       </ScrollView>
       <NextButton onPressNext={onPressNext} />
@@ -37,5 +49,8 @@ export const WriteGoodsOnline = () => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
+  },
+  switch: {
+    marginBottom: 10,
   },
 })
