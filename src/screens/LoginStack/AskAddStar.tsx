86f38e09436d4a50ec6d511e32efb9f590ff5b53
kauthenticity <kauthenticity@gmail.com>
2022-06-28 23:17:53 +0900

    [feat]: 카테고리 문의하기 페이지에 생년월일 date picker 추가

--- src/screens/LoginStack/AskAddStar.tsx
+++ src/screens/LoginStack/AskAddStar.tsx
@@ -1,45 +1,83 @@
 import React, {useCallback, useState} from 'react'
-import {View, Text, TextInput, Animated, Pressable, StyleSheet} from 'react-native'
+import {View, Text, TextInput, Animated, Pressable, Alert, Platform, StyleSheet} from 'react-native'
 import {SafeAreaView} from 'react-native-safe-area-context'
 import type {Asset} from 'react-native-image-picker'
 import {launchImageLibrary} from 'react-native-image-picker'
 import {useNavigation} from '@react-navigation/native'
 import FastImage from 'react-native-fast-image'
+import KeyboardManager from 'react-native-keyboard-manager'
+import DatePicker from 'react-native-date-picker'
+import moment from 'moment'
+
 import {useToggle, useAnimatedValue} from '../../hooks'
-import {StackHeader, DownArrowIcon, RoundButton, PlusIcon, RemoveButtonIcon} from '../../components/utils'
+import {StackHeader, DownArrowIcon, RoundButton, PlusIcon, RemoveButtonIcon, NeccesaryField} from '../../components/utils'
 import * as theme from '../../theme'
+
+// ***************************** ios keyboard settings *****************************
+if (Platform.OS === 'ios') {
+  KeyboardManager.setEnable(true)
+  KeyboardManager.setEnableDebugging(false)
+  KeyboardManager.setKeyboardDistanceFromTextField(10)
+  KeyboardManager.setLayoutIfNeededOnUpdate(true)
+  KeyboardManager.setEnableAutoToolbar(false)
+  KeyboardManager.setToolbarDoneBarButtonItemText('확인')
+  KeyboardManager.setToolbarManageBehaviourBy('subviews') // "subviews" | "tag" | "position"
+  KeyboardManager.setToolbarPreviousNextButtonEnable(false)
+  KeyboardManager.setToolbarTintColor('#007aff') // Only #000000 format is supported
+  KeyboardManager.setToolbarBarTintColor('#FFFFFF') // Only #000000 format is supported
+  KeyboardManager.setShouldShowToolbarPlaceholder(true)
+  KeyboardManager.setOverrideKeyboardAppearance(false)
+  KeyboardManager.setKeyboardAppearance('default') // "default" | "light" | "dark"
+  KeyboardManager.setShouldResignOnTouchOutside(true)
+  KeyboardManager.setShouldPlayInputClicks(true)
+  KeyboardManager.resignFirstResponder()
+}
 
 const IMAGE_SIZE = 64
 
+// ******************** email Validation  ********************
+function validateEmail(email: string) {
+  var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+  if (email.match(validRegex)) {
+    return true
+  } else {
+    return false
+  }
+}
+
 export const AskAddStar = () => {
+  // ******************** states  ********************
   const navigation = useNavigation()
   const [opend, toggleOpend] = useToggle(false) // 카테고리 select 토글
   const [mainCategory, setMainCategory] = useState<'가수' | '배우'>('가수') // 등록할 카테고리
   const [name, setName] = useState<string>('') // 이름 혹은 그룹명
   const [image, setImage] = useState<Asset>() // 공식 대표 이미지
   const [email, setEmail] = useState<string>('') // 연락 받을 이메일
-  const animatedValue = useAnimatedValue()
+  const [date, setDate] = useState<Date>(new Date())
+  const [modalOpen, setModalOpen] = useState(false)
 
+  // ******************** animations  ********************
+  const animatedValue = useAnimatedValue()
   const open = Animated.timing(animatedValue, {
     // Select box 토글 애니메이션
     toValue: opend == true ? 0 : 1,
     duration: 200,
     useNativeDriver: false,
   })
-
   const animatedHeight = animatedValue.interpolate({
     // select box 높이
     inputRange: [0, 1],
     outputRange: [0, theme.INPUT_HEIGHT],
     extrapolate: 'clamp',
   })
-
   const animatedRotation = animatedValue.interpolate({
     // select arrow 회전 애니메이션
     inputRange: [0, 1],
     outputRange: ['0deg', '180deg'],
   })
 
+  // ******************** callbacks  ********************
   const onPressOpen = useCallback(() => {
     // select box누르면 애니메이션 시작
     open.start(toggleOpend)
@@ -57,12 +95,20 @@ export const AskAddStar = () => {
   }, [])
 
   const onPressSubmit = useCallback(() => {
+    if (email != '' && !validateEmail(email)) {
+      Alert.alert('이메일을 확인해주세요', '', [
+        {
+          text: '확인',
+        },
+      ])
+      return
+    }
     const form = {mainCategory, name, image, email}
 
     // 백단으로 문의하기 게시글 post api
 
     navigation.navigate('AskAddStarComplete')
-  }, [mainCategory, name, image, email])
+  }, [mainCategory, name, image, email, date])
 
   // 라이브러리에서 이미지 선택했을 때 호출되는 callback
   const onImageLibraryPress = useCallback(async () => {
@@ -80,7 +126,7 @@ export const AskAddStar = () => {
 
   // 필수 항목 전부 채웠는지 확인
   const checkEnabled = useCallback(() => {
-    if (name == '' || email == '') {
+    if (name == '') {
       return false
     } else {
       return true
@@ -92,7 +138,10 @@ export const AskAddStar = () => {
       <StackHeader title="문의하기" goBack />
       <View style={{padding: theme.PADDING_SIZE}}>
         <View style={styles.spacing}>
-          <Text style={[theme.styles.label]}>등록할 카테고리</Text>
+          <View style={[theme.styles.rowFlexStart]}>
+            <Text style={[theme.styles.label]}>등록할 카테고리</Text>
+            <NeccesaryField />
+          </View>
           <Pressable
             onPress={onPressOpen}
             style={[
@@ -124,7 +173,11 @@ export const AskAddStar = () => {
           </Pressable>
         </View>
         <View style={styles.spacing}>
-          <Text style={[theme.styles.label]}>이름 혹은 그룹명</Text>
+          <View style={[theme.styles.rowFlexStart]}>
+            <Text style={[theme.styles.label]}>이름 혹은 그룹명</Text>
+            <NeccesaryField />
+          </View>
+
           <TextInput
             value={name}
             onChangeText={setName}
@@ -132,6 +185,37 @@ export const AskAddStar = () => {
             placeholderTextColor={theme.gray300}
             style={[theme.styles.input]}
           />
+        </View>
+        <View style={styles.spacing}>
+          <View style={[theme.styles.rowFlexStart]}>
+            <Text style={[theme.styles.label]}>생년월일</Text>
+            <NeccesaryField />
+          </View>
+
+          <DatePicker
+            modal
+            open={modalOpen}
+            date={date}
+            mode={'date'}
+            onConfirm={date => {
+              setModalOpen(false)
+              setDate(date)
+            }}
+            onCancel={() => {
+              setModalOpen(false)
+            }}
+          />
+          <Pressable onPress={() => setModalOpen(true)}>
+            <TextInput
+              onPressIn={() => setModalOpen(true)}
+              editable={false}
+              value={moment(date).format('YYYY년 MM월 DD일')}
+              placeholder="동명이인 구분을 위해 생년월일을 입력해 주세요."
+              placeholderTextColor={theme.gray300}
+              style={[theme.styles.input]}
+            />
+            <DownArrowIcon style={{position: 'absolute', right: 16, top: 12}} />
+          </Pressable>
         </View>
         <View style={[styles.spacing]}>
           <Text style={[theme.styles.label]}>공식 대표 이미지</Text>
--- src/screens/LoginStack/AskAddStarComplete.tsx
+++ src/screens/LoginStack/AskAddStarComplete.tsx
@@ -1,31 +1,42 @@
-import React, {useCallback} from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import React, {useState, useMemo, useCallback} from 'react'
+import {View, Text, StyleSheet, Dimensions} from 'react-native'
 import {SafeAreaView} from 'react-native-safe-area-context'
-import FastImage from 'react-native-fast-image'
+import {getStatusBarHeight} from 'react-native-status-bar-height'
+import {isIphoneX, getBottomSpace} from 'react-native-iphone-x-helper'
 import type {NativeStackNavigationProp} from '@react-navigation/native-stack'
 import {useNavigation} from '@react-navigation/native'
-import {StackHeader, RoundButton} from '../../components/utils'
+import {StackHeader, RoundButton, CompleteIcon} from '../../components/utils'
 import * as theme from '../../theme'
 
 export const AskAddStarComplete = () => {
+  const [height, setHeight] = useState<number>(0)
+
+  const MARGIN_TOP = useMemo(() => {
+    const status = getStatusBarHeight()
+    if (isIphoneX()) {
+      return (Dimensions.get('window').height - status - getBottomSpace() - height) / 2
+    } else {
+      return (Dimensions.get('window').height - status - height) / 2
+    }
+  }, [height])
+
   const navigation = useNavigation<NativeStackNavigationProp<any>>()
   const onPressNext = useCallback(() => {
-    // navigation.navigate('SelectCategory', {
-    //   email: '',
-    //   name: '',
-    // })
     navigation.pop(2)
   }, [])
   return (
     <SafeAreaView style={theme.styles.safeareaview}>
       <StackHeader title="문의하기" goBack />
-      <View style={{flex: 1, padding: theme.PADDING_SIZE, justifyContent: 'center', alignItems: 'center'}}>
-        <View>
-          <FastImage source={{uri: 'http://localhost:8081/src/assets/images/Complete.png'}} style={{width: 53, height: 53, alignSelf: 'center'}} />
+      <View style={[styles.container, {top: MARGIN_TOP}]}>
+        <View
+          onLayout={e => {
+            setHeight(e.nativeEvent.layout.height)
+          }}>
+          <CompleteIcon style={{alignSelf: 'center'}} />
           <View style={{marginVertical: 32, alignItems: 'center'}}>
             <Text style={[theme.styles.bold20, {marginBottom: 8}]}>문의하기 등록 완료</Text>
             <Text style={{fontSize: 16, color: theme.gray700, textAlign: 'center'}}>
-              기존 카테고리 내에 중복확인 후, 결과를 이메일로 전달드리겠습니다. 감사합니다.
+              기존 카테고리 내에 중복확인 후, 결과를 이메일로 전달해 드리겠습니다. 감사합니다.
             </Text>
           </View>
           <RoundButton label="카테고리로 이동" enabled onPress={onPressNext} />
@@ -35,4 +46,13 @@ export const AskAddStarComplete = () => {
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    //flex: 1,
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: Dimensions.get('window').width,
+    paddingHorizontal: theme.PADDING_SIZE,
+  },
+})
