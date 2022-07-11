3814de32c72446fb45f888c595acf80fc0053a9e
kauthenticity <kauthenticity@gmail.com>
2022-07-12 01:54:54 +0900

    [feat]: 찜 스크린 찜한 게시글이 없을 때

--- src/screens/MainTab/Favorites.tsx
+++ src/screens/MainTab/Favorites.tsx
@@ -15,7 +15,7 @@ type CategoryItemProps = {
   selectedCategory: string
   currentIndex: number
   selectedIndex: number
-  scrollRef: React.RefObject<ScrollView>
+  scrollRef: React.RefObject<FlatList>
 }
 
 const CategoryItem = ({name, id, onPressCategory, selectedCategory, currentIndex, selectedIndex, scrollRef}: CategoryItemProps) => {
@@ -31,7 +31,9 @@ const CategoryItem = ({name, id, onPressCategory, selectedCategory, currentIndex
 
 export const Favorites = () => {
   // ******************** utils ********************
-  const scrollRef = useRef<ScrollView>(null)
+  const scrollRef = useRef<FlatList>(null)
+  const user = useAppSelector(state => state.auth.user)
+
   // ******************** states ********************
   const [selectedCategory, setSelectedCategory] = useState<string>('0') // 선택된 카테고리의 id값 저장. (처음엔 전체의 id값)
   const [sharings, setSharings] = useState<INanumListItem[]>([]) // 나눔 리스트
@@ -39,86 +41,75 @@ export const Favorites = () => {
   const [selectedIndex, setSelectedIndex] = useState<number>(0)
   const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn)
 
-  useEffect(() => {
-    fetch('http://localhost:8081/src/data/dummySharings.json')
-      .then(res => res.json())
-      .then(result => {
-        setSharings(result)
-      })
-  }, [])
-
   const onPressCategory = useCallback((id: string, currentIndex: number) => {
     setSelectedCategory(id)
     setSelectedIndex(currentIndex)
+    scrollRef.current?.scrollToIndex({index: currentIndex})
     // 해당 id에 일치하는 나눔 리스트만 state에 저장.
   }, [])
 
   // refresh pull up 하면 서버에서 가져옴
-  const onRefresh = useCallback(() => {
-    setRefreshing(true)
-    fetch('http://localhost:8081/src/data/dummyNanums.json')
-      .then(res => res.json())
-      .then(result => {
-        setSharings(result)
-        setRefreshing(false)
-      })
-  }, [])
+  const onRefresh = useCallback(() => {}, [])
 
   return (
     <SafeAreaView style={[theme.styles.safeareaview]} edges={['top', 'left', 'right']}>
       <StackHeader title="찜한 리스트" />
       {isLoggedIn ? (
-        <View>
+        <View style={{flex: 1}}>
           <View style={styles.container}>
-            <ScrollView ref={scrollRef} horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{marginBottom: 10}}>
-              <CategoryItem
-                currentIndex={0}
-                selectedIndex={selectedIndex}
-                id="0"
-                name="전체"
-                onPressCategory={onPressCategory}
-                selectedCategory={selectedCategory}
-                scrollRef={scrollRef}
-              />
-              <CategoryItem
-                currentIndex={1}
-                selectedIndex={selectedIndex}
-                id="1"
-                name="BTS"
-                onPressCategory={onPressCategory}
-                selectedCategory={selectedCategory}
-                scrollRef={scrollRef}
-              />
-              <CategoryItem
-                selectedIndex={selectedIndex}
-                currentIndex={2}
-                id="2"
-                name="세븐틴"
-                onPressCategory={onPressCategory}
-                selectedCategory={selectedCategory}
-                scrollRef={scrollRef}
-              />
-              <CategoryItem
-                selectedIndex={selectedIndex}
-                currentIndex={3}
-                id="3"
-                name="아이브"
-                onPressCategory={onPressCategory}
-                selectedCategory={selectedCategory}
-                scrollRef={scrollRef}
-              />
-            </ScrollView>
+            <FlatList
+              ref={scrollRef}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{marginBottom: 10}}
+              data={[{category: '전체보기'}].concat(user.accountCategoryDtoList)}
+              renderItem={({item, index}) =>
+                index == 0 ? (
+                  <CategoryItem
+                    name={'전체 보기'}
+                    currentIndex={index}
+                    selectedIndex={selectedIndex}
+                    id={index.toString()}
+                    onPressCategory={onPressCategory}
+                    selectedCategory={selectedCategory}
+                    scrollRef={scrollRef}
+                  />
+                ) : (
+                  <CategoryItem
+                    name={item.category}
+                    currentIndex={index}
+                    selectedIndex={selectedIndex}
+                    id={index.toString()}
+                    onPressCategory={onPressCategory}
+                    selectedCategory={selectedCategory}
+                    scrollRef={scrollRef}
+                  />
+                )
+              }></FlatList>
           </View>
 
-          <FlatList
-            contentContainerStyle={{paddingHorizontal: theme.PADDING_SIZE, paddingVertical: 10}}
-            data={sharings}
-            renderItem={({item}) => <NanumListItem item={item}></NanumListItem>}
-            refreshing={refreshing}
-            numColumns={2}
-            columnWrapperStyle={{justifyContent: 'space-between', marginBottom: 20}}
-            onRefresh={onRefresh}
-          />
+          {sharings.length > 0 ? (
+            <FlatList
+              contentContainerStyle={{paddingHorizontal: theme.PADDING_SIZE, paddingVertical: 10}}
+              data={sharings}
+              renderItem={({item}) => <NanumListItem item={item}></NanumListItem>}
+              refreshing={refreshing}
+              numColumns={2}
+              columnWrapperStyle={{justifyContent: 'space-between', marginBottom: 20}}
+              onRefresh={onRefresh}
+            />
+          ) : (
+            <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+              <EmptyIcon style={{marginBottom: 32}} />
+              <View>
+                <Text style={[theme.styles.bold20, {marginBottom: 8}]}>현재 찜 리스트가 비어있어요.</Text>
+                <View>
+                  <Text style={[{color: theme.gray700, fontSize: 16, textAlign: 'center'}, theme.styles.text16]}>관심 있는, 오픈 예정인 나눔을</Text>
+                  <Text style={[{color: theme.gray700, fontSize: 16, textAlign: 'center'}, theme.styles.text16]}>찜 리스트에 추가해보세요!</Text>
+                </View>
+              </View>
+            </View>
+          )}
         </View>
       ) : (
         <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
