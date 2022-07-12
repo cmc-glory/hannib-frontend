6eff020b3c878d099205703a370a450c11af901a
Yejin Kim <qja086351@naver.com>
2022-07-12 20:17:33 +0900

    [design]: 참여, 진행 나눔 없는 경우 화면 추가

--- src/screens/MyPageTabStack/ParticipatingSharingList.tsx
+++ src/screens/MyPageTabStack/ParticipatingSharingList.tsx
@@ -2,7 +2,7 @@ import React, {useState, useCallback, useEffect} from 'react'
 import {View, FlatList, Text, StyleSheet} from 'react-native'
 import {SafeAreaView} from 'react-native-safe-area-context'
 import {useAppSelector} from '../../hooks'
-import {StackHeader} from '../../components/utils'
+import {EmptyIcon, StackHeader} from '../../components/utils'
 import {ParticipatingSharingItem} from '../../components/MyPageTabStack'
 import * as theme from '../../theme'
 import {ISharingInfo} from '../../types'
@@ -18,7 +18,8 @@ export const ParticipatingSharingList = () => {
     fetch('http://localhost:8081/src/data/dummySharings.json')
       .then(res => res.json())
       .then(result => {
-        setSharings(result)
+        // setSharings(result)
+        setSharings([])
       })
   }, [])
 
@@ -28,7 +29,8 @@ export const ParticipatingSharingList = () => {
     fetch('http://localhost:8081/src/data/dummySharings.json')
       .then(res => res.json())
       .then(result => {
-        setSharings(result)
+        // setSharings(result)
+        setSharings([])
         setRefreshing(false)
       })
   }, [])
@@ -37,15 +39,28 @@ export const ParticipatingSharingList = () => {
     <SafeAreaView style={theme.styles.safeareaview} edges={['top', 'left', 'right']}>
       <StackHeader title="참여한 나눔" goBack />
       <View style={styles.container}>
-        <FlatList
-          contentContainerStyle={{paddingHorizontal: theme.PADDING_SIZE, paddingVertical: 10}}
-          data={sharings}
-          renderItem={({item}) => <ParticipatingSharingItem item={item}></ParticipatingSharingItem>}
-          refreshing={refreshing}
-          numColumns={2}
-          columnWrapperStyle={{justifyContent: 'space-between', marginBottom: 20}}
-          onRefresh={onRefresh}
-        />
+        {sharings.length == 0 ? (
+          <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+            <EmptyIcon style={{marginBottom: 32}} />
+            <View>
+              <Text style={[theme.styles.bold20, {marginBottom: 8, textAlign: 'center'}]}>아직 참여한 나눔이 없어요.</Text>
+              <View>
+                <Text style={[{color: theme.gray700, fontSize: 16, textAlign: 'center'}, theme.styles.text16]}>리스트 페이지에서 나눔을 구경하고 </Text>
+                <Text style={[{color: theme.gray700, fontSize: 16, textAlign: 'center'}, theme.styles.text16]}>참여하여 굿즈를 수령해보세요!</Text>
+              </View>
+            </View>
+          </View>
+        ) : (
+          <FlatList
+            contentContainerStyle={{paddingHorizontal: theme.PADDING_SIZE, paddingVertical: 10}}
+            data={sharings}
+            renderItem={({item}) => <ParticipatingSharingItem item={item}></ParticipatingSharingItem>}
+            refreshing={refreshing}
+            numColumns={2}
+            columnWrapperStyle={{justifyContent: 'space-between', marginBottom: 20}}
+            onRefresh={onRefresh}
+          />
+        )}
       </View>
     </SafeAreaView>
   )
