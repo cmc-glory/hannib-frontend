99ce3f7e76a24378f320d96d603e7d23eb6aa6da
Yejin Kim <qja086351@naver.com>
2022-07-13 19:05:06 +0900

    [feat]: deep linking ios 셋팅

--- ios/hannib/AppDelegate.mm
+++ ios/hannib/AppDelegate.mm
@@ -46,6 +46,13 @@
                   continueUserActivity:userActivity
                     restorationHandler:restorationHandler];
 }
+- (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity
+ restorationHandler:(void (^)(NSArray * _Nullable))restorationHandler
+{
+  return [RCTLinkingManager application:application
+                   continueUserActivity:userActivity
+                     restorationHandler:restorationHandler];
+}
 @end
 #endif
 
