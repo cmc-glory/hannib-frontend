8f3cc4976f1fafdf64c70b940cfadbe398277827
Yejin Kim <qja086351@naver.com>
2022-07-15 03:28:05 +0900

    [design]: 나눔 신청 완료 페이지 디자인 수정

--- src/screens/GoodsStack/GoodsRequestComplete.tsx
+++ src/screens/GoodsStack/GoodsRequestComplete.tsx
@@ -1,11 +1,10 @@
 import React, {useState, useMemo, useCallback} from 'react'
 import {View, Text, StyleSheet, Dimensions} from 'react-native'
-import {useNavigation} from '@react-navigation/native'
+import {useNavigation, useRoute} from '@react-navigation/native'
 import {SafeAreaView} from 'react-native-safe-area-context'
 import {getStatusBarHeight} from 'react-native-status-bar-height'
 import {isIphoneX, getBottomSpace} from 'react-native-iphone-x-helper'
 
-import {StepIndicator} from '../../components/WriteGoodsStack'
 import {StackHeader, CompleteIcon, RoundButton} from '../../components/utils'
 import * as theme from '../../theme'
 
@@ -22,14 +21,15 @@ export const GoodsRequestComplete = () => {
   }, [height])
 
   const onPressButton = useCallback(() => {
-    navigation.navigate('GoodsDetail')
+    navigation.navigate('MyPageTabStackNavigator', {
+      screen: 'ParticipatingSharingList',
+    })
   }, [])
   return (
     <SafeAreaView style={theme.styles.safeareaview}>
       <StackHeader title="신청하기" goBack />
 
       <View style={{paddingHorizontal: theme.PADDING_SIZE}}>
-        <StepIndicator step={3} />
         <View
           style={[styles.container, {top: MARGIN_TOP}]}
           onLayout={e => {
@@ -37,11 +37,11 @@ export const GoodsRequestComplete = () => {
           }}>
           <CompleteIcon />
           <View style={{marginVertical: 32, alignItems: 'center'}}>
-            <Text style={[theme.styles.bold20, {marginBottom: 8}]}>모집글 등록 완료</Text>
-            <Text style={{color: theme.gray700, fontSize: 16}}>모집글 작성이 완료됐습니다.</Text>
-            <Text style={{color: theme.gray700, fontSize: 16}}>게시글이 잘 등록됐는지 확인해보세요!</Text>
+            <Text style={[theme.styles.bold20, {marginBottom: 8}]}>신청 완료</Text>
+            <Text style={{color: theme.gray700, fontSize: 16}}>나눔 신청이 완료됐습니다.</Text>
+            <Text style={{color: theme.gray700, fontSize: 16}}>신청 내역에서 확인해보세요!</Text>
           </View>
-          <RoundButton label="등록한 게시글로 이동" style={{alignSelf: 'stretch'}} enabled onPress={onPressButton} />
+          <RoundButton label="신청 내역으로 이동" style={{alignSelf: 'stretch'}} enabled onPress={onPressButton} />
         </View>
       </View>
     </SafeAreaView>
