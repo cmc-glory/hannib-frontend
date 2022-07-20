808f50d38d9e8da54997ff544df37ceae1bb887b
Yejin Kim <qja086351@naver.com>
2022-07-21 02:05:00 +0900

    [feat]: 안드로이드 릴리스 관련 파일들

--- android/app/src/main/java/com/hannib/MainActivity.java
+++ android/app/src/main/java/com/hannib/MainActivity.java
@@ -1,8 +1,10 @@
 package com.hannib;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+//import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
